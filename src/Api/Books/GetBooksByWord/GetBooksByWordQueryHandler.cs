0829d6448588eba1e8c67d28cae485ee25f58a52
dzh <d.snitchester@gmail.com>
2026-08-22 19:35:28 +0300

    031. *ТЗ* рефакторинг

--- src/Api/Books/GetBooksByWord/GetBooksByWordQueryHandler.cs
+++ src/Api/Books/GetBooksByWord/GetBooksByWordQueryHandler.cs
@@ -3,14 +3,13 @@ namespace Catalog.Api.Books.GetBooksByWord;
 // модель запроса (DTO-модель)
 public record GetBooksByWordQuery(int? PageNumber = 1, int? PageSize = 5, string Term = "") : IQuery<GetBooksByWordResult>;
 
-public class GetBooksByWordQueryQueryValidator : AbstractValidator<GetBooksByWordQuery>
+public class GetBooksByWordQueryValidator : AbstractValidator<GetBooksByWordQuery>
 {
-	public GetBooksByWordQueryQueryValidator()
+	public GetBooksByWordQueryValidator()
 	{
 		RuleFor(item => item.Term)
-			.NotEmpty().WithMessage("Term не может быть пустым")
-			.Must(item => item!.Length > 2)
-			.WithMessage("Длина Term должна быть больше 2 символов");
+			.NotEmpty()
+			.MinimumLength(2);
 	}
 }
 
@@ -24,17 +23,17 @@ public class GetBooksByWordQueryHandler
 		IDocumentSession session,
 		CancellationToken cancellationToken)
 	{
-		var word = query.Term.ToLower();
+		var word = query.Term;
 
 		var books = await session.Query<Book>()
-			.Where(item => item.Title.Contains(word, StringComparison.CurrentCultureIgnoreCase) ||
-				item.Name.Contains(word, StringComparison.CurrentCultureIgnoreCase) ||
-				item.Description.Contains(word, StringComparison.CurrentCultureIgnoreCase))
+			.Where(item => item.Title.Contains(word, StringComparison.OrdinalIgnoreCase) ||
+				item.Name.Contains(word, StringComparison.OrdinalIgnoreCase) ||
+				item.Description.Contains(word, StringComparison.OrdinalIgnoreCase))
 			.ToPagedListAsync(
 				query.PageNumber ?? 1,
 				query.PageSize ?? 5,
 				cancellationToken);
 
-		return !books.IsEmpty() ? new GetBooksByWordResult(books) : new GetBooksByWordResult([]);
+		return new GetBooksByWordResult(books);
 	}
 }
