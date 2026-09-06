5393f6ecf3955fd2cff6d64e3214d4bf339e086d
dzh <d.snitchester@gmail.com>
2026-09-06 21:49:23 +0300

    039. .NET Aspire: топология приложения кодом на C#. Ручной шаблон библиотеки ServiceDefaults (описание того, что должно быть в каждом сервисе, чтобы наблюдать за ним)

---
+++ src/ServiceDefaults/Extensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.Hosting; // неявно подключен в любом веб-проекте,
+																				// для доступа из любого Program.cs (по шаблону Aspire)
+
+// метод-расширения для использования в API
+public static class Extensions
+{
+	// метод обобщенный для доступности в любом сервисе решения (не только веб-API, но и воркеры/консольные службы)
+	public static TBuilder AddServiceDefaults<TBuilder>(this TBuilder builder)
+		where TBuilder : IHostApplicationBuilder // общий интерфейс всех строителей хоста в .NET
+	{
+		return builder; // fluent-стиль, возврат полученного билдера, чтобы вызовы можно было выстраивать в цепочку
+	}
+}
